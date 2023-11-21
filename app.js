1c49dd6b14c7ee420bef4e68bd34681fd732bf86
AbhijitPramanick <amanik1995@gmail.com>
2023-11-21 14:36:41 +0530

    Handling Responses

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  //   console.log(req); // This req object is an object that Node JS created for us with all the incoming data, when we visited the localhost:3000
-
-  //It contains Headers that are metadata, added to a request and also to response
-
-  console.log(req.url, req.method, req.headers);
+  ///////Handling responses
+  res.setHeader("Content-Type", "text/html");
+  res.write(`<html>`);
+  res.write(`<header><title>Handling responses</title></header>`);
+  res.write(`<body><h1>Hello from Node.js server!</h1></body>`);
+  res.write(`</html>`);
+  res.end();
 });
 server.listen(3000);
