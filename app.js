f3cf699255556041462a53dd4f6725516a9de2fd
AbhijitPramanick <amanik1995@gmail.com>
2023-11-21 13:33:16 +0530

    Understanding requests

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  //   process.exit(); //Quitting your server, hard exiting event loop
+  //   console.log(req); // This req object is an object that Node JS created for us with all the incoming data, when we visited the localhost:3000
+
+  //It contains Headers that are metadata, added to a request and also to response
+
+  console.log(req.url, req.method, req.headers);
 });
 server.listen(3000);
