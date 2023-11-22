5d3a56ab7d6c18febbbdee72a979f94c3adb5580
AbhijitPramanick <amanik1995@gmail.com>
2023-11-22 15:48:14 +0530

    Understanding Middleware and ExpressJS

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
+//Following is a middleware, it will recieve a function, that will run on every incoming request
+app.use((req, res, next) => {
+  console.log(`You are in a middleware.`);
+  next(); //this allows the request to continue to the next middleware bin line
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log(`You are in another middleware.`);
+  res.send(`<h1>Hello from express JS !</h1>`);
+});
+
+// const server = http.createServer(app);
+//
+// server.listen(3000);
+
+app.listen(3000);
