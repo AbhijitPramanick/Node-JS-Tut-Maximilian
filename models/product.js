3df0bb9ed3a225b05b6999468679e24d8216d72e
AbhijitPramanick <amanik1995@gmail.com>
2023-11-29 14:09:15 +0530

    Storing and fetching data from files via Models

--- models/product.js
+++ models/product.js
@@ -1,12 +1,48 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
 module.exports = class product{
     constructor(t){
         this.title= t;
     }
     save(){
-        products.push(this);
+        const p = path.join(path.dirname(process.mainModule.filename),
+        'data',
+        'products.json');
+        fs.readFile(p,(err,fileContent)=>{
+            let products = [];
+            // if(!err){
+            //     products = JSON.parse(fileContent);
+            // }
+            if (!err) {
+                try {
+                    products = JSON.parse(fileContent);
+                } catch (parseError) {
+                    console.log('Error parsing JSON:', parseError);
+                }
+            }
+
+            products.push(this);
+            fs.writeFile(p,JSON.stringify(products),(err)=>{
+                console.log(err);
+            })
+        });
     }
-    static fetchAll(){
-        return products;
+    static fetchAll(cb){
+        const p = path.join(path.dirname(process.mainModule.filename),
+        'data',
+        'products.json');
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                cb([]);
+            } else {
+                try {
+                    cb(JSON.parse(fileContent));
+                } catch (parseError) {
+                    console.log('Error parsing JSON:', parseError);
+                    cb([]);
+                }
+            }
+        });
     }
 }
